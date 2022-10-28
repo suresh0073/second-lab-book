ba2aa9b4d53db7e11c5994a1d14a343ef7648545
suresh0073 <114458591+suresh0073@users.noreply.github.com>
2022-10-28 11:34:04 +0530

    Add files via upload

---
+++ PrintANumber.java
@@ -0,0 +1,12 @@
+package secondlabbool;
+
+import java.util.Scanner;
+public class PrintANumber {
+    public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		 System.out.print("Enter a number: ");
+		int number = reader.nextInt();
+		System.out.println("You entered: " + number);
+    }
+    
+} 
---
+++ PrintAsciiValue.java
@@ -0,0 +1,11 @@
+package secondlabbool;
+
+public class PrintAsciiValue {
+	 public static void main(String[] args) {
+	 char ch = 'H';
+	 int ascii = ch;
+	int castAscii = (int) ch;
+	 System.out.println("The ASCII value of " + ch + " is: " + ascii);  
+	 System.out.println("The ASCII value of " + ch + " is: " + castAscii);  
+	 }
+	}
